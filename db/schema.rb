836c6d0947d9db6be666f32bee576c199ca745ba
ShahanaMay <shahana@codelation.com>
2014-08-18 10:53:52 -0500

    generate project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818154513) do
+ActiveRecord::Schema.define(version: 20140818155236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140818154513) do
     t.string   "name"
     t.string   "company_name"
     t.string   "email_address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
