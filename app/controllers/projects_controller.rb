bee50167bcfe07fac95c53677fe97b1a252a61c4
ShahanaMay <shahana@codelation.com>
2014-08-18 18:47:20 -0500

    create projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,13 +4,52 @@ class ProjectsController < ApplicationController
     @title = "Projects"
     @project = Project.first
   end
-
-  def show
-  end
-
   def new
+    @title = "New Project"
+    @project = Project.new
   end
 
   def edit
+    @title = "Edit Project"
+    @project = Project.find(params[:id])
   end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Project was successfully saved. Thank you!"
+    else
+      render :new, alert: "Project was not saved"
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project was successfully updated. Thank you!"
+    else
+      render :new, alert: "Project was not updated"
+    end
+  end
+
+  def destroy
+    @project = Project.find_by_id(params[:id])
+    if @project
+      @project.destroy
+    end
+    redirect_to project_path
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @title = @project.name
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:description, :name, :client_id)
+  end
+
 end
